f7d0d008a6b45bb807b8998f07a8353f78962463
ruoyaow <ruoyaow@umich.edu>
2019-11-07 21:37:38 -0500

    new feature

--- noun.py
+++ noun.py
@@ -3,11 +3,17 @@ import nltk
 # nlkt.download('punkt')
 # nlkt.download('averaged_perceptron_tagger')
 
+word_dict = {}
+
 def gen(text, parsed_sentence, outputFile):
     with open(outputFile, 'w') as f:
         for i in range(len(parsed_sentence)):
             if parsed_sentence[i][1] == 'NN':
                 new_sentence = text[:]
+                if new_sentence[i] in word_dict:
+                    word_dict[new_sentence[i]] += 1
+                else:
+                    word_dict[new_sentence[i]] = 1
                 new_sentence[i] = '[MASK]'                
                 f.write(' '.join(new_sentence))
                 f.write('\n')
