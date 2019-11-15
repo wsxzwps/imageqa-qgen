ee58de94d803bdebb6ac6fd5eed29f7f85f47e18
ruoyaow <ruoyaow@umich.edu>
2019-11-14 22:03:54 -0500

    plot

--- evaluation/draw.py
+++ evaluation/draw.py
@@ -10,10 +10,10 @@ num = 40
 x = [s[0] for s in sorted_words[:num]]
 y = [s[1] for s in sorted_words[:num]]
 
-x_pos = [i*2 for i, _ in enumerate(x)]
+x_pos = [i for i, _ in enumerate(x)]
 x_pos.reverse()
 
-plt.barh(x_pos, y, color='#004080')
+plt.barh(x_pos, y, color='#004080', width=0.5)
 plt.ylabel("Word Frequency")
 plt.title("Masked Nouns Frequency")
 
