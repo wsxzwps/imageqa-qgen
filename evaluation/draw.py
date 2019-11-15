41a542aaaa52b7ff01ed520f2d53c4d2675fd846
ruoyaow <ruoyaow@umich.edu>
2019-11-14 21:28:53 -0500

    plot

--- evaluation/draw.py
+++ evaluation/draw.py
@@ -5,16 +5,16 @@ with open('nouns.pkl', 'rb') as f:
     data = pickle.load(f)
 
 sorted_words = sorted(data.items(), key= lambda k : (k[1], k[0]), reverse=True)
-num = 20
+num = 40
 
 x = [s[0] for s in sorted_words[:num]]
 y = [s[1] for s in sorted_words[:num]]
 
 x_pos = [i for i, _ in enumerate(x)]
 
-plt.barh(x_pos, y, color='blue')
+plt.barh(x_pos, y, color='cyan')
 plt.ylabel("Word Frequency")
 plt.title("Masked Nouns Frequency")
 
-plt.xticks(x_pos, x)
+plt.yticks(x_pos, x)
 plt.savefig('wordfrequency.jpg')
